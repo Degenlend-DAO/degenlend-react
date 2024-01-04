17cdf90ea307bfc640c995eeb073206dbefe235e
Kevin Kamto <kamtokevin@gmail.com>
2024-01-03 23:07:06 -0500

    chore: basic supply markets component

---
+++ src/components/Markets/supply_markets.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SupplyMarkets = () => {
+    <div>
+        This is the supply markets including SX token, and eventually AX token
+    </div>
+}
+
+export default SupplyMarkets
