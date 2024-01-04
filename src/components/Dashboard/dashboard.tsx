cfac6f669fdd147947b1358c83b8694dd733bba8
Kevin Kamto <kamtokevin@gmail.com>
2024-01-04 17:01:22 -0500

    refactor: setup basic dashboard

--- src/components/Dashboard/dashboard.tsx
+++ src/components/Dashboard/dashboard.tsx
@@ -1,40 +1,54 @@
 import React from 'react';
-import { Progress } from 'antd';
+import { Card, Col, Row, Statistic, Progress } from 'antd';
 
 
-const Dashboard = () => {
+
+const Dashboard = () => <>{
 <div>
-    <div className="row">
-        <div className="column">
-            <h3>
-                Supply Balance
-            </h3>
-            <p>
-                0%
-            </p>
-        </div>
+    <Row gutter={16}>
+        <Col span={6}>
+            <Card bordered={false}>
+            <Statistic
+                title="Supply Balance"
+                value={11.28}
+                precision={2}
+                valueStyle={{ color: '#3f8600' }}
+                prefix="$"
+            />
+            </Card>
+        </Col>
+        <Col span={6}>
+            <Card bordered={false}>
+            <Statistic
+                title="Net APY"
+                value={0}
+                precision={2}
+                valueStyle={{ color: 'black' }}
+                suffix="%"
+            />
+            </Card>
+        </Col>
 
-        <div className="column">
-            Net APY
-        </div>
-        <div className="column">
-            <h3>
-            Borrow Balance
-            </h3>
-            <p>
-                0%
-            </p>
-        </div>
-    </div>
-    <div className='row'>
-        <div>
-        Borrow Limit
-            <div>
-                <Progress percent={30} status="active" />
-            </div>
-        </div>
-    </div>
+        <Col span={6}>
+            <Card bordered={false}>
+            <Statistic
+                title="Borrow Balance"
+                value={20}
+                precision={2}
+                valueStyle={{ color: '#3f8600' }}
+                prefix="$"
+            />
+            </Card>
+        </Col>
+    </Row>
+    <Row gutter={12} >
+        <Col span={12}>
+        <Card bordered={true} title="Borrow Limit">
+                    <Progress percent={30} status="active" />
+                </Card>
+        </Col>
+    </Row>
 </div>
-}
+}</>
 
 export default Dashboard
