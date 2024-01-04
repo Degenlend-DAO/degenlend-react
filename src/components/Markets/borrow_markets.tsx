eeed1f877f7e65e2a1fcfaf9f80d9fcf2bbeca4a
Kevin Kamto <kamtokevin@gmail.com>
2024-01-04 12:15:57 -0500

    chore: borrow markets structure setup

--- src/components/Markets/borrow_markets.tsx
+++ src/components/Markets/borrow_markets.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
 const BorrowMarkets = () => {
-    <div>
-        This is the borrow markets, where you can borrow USDC
+    <div className="column">
+        <h3>
+            Borrow Markets
+        </h3>
+        <div className="row">
+            <a>
+                Asset
+            </a>
+            <a>
+                APY
+            </a>
+            <a>
+                Wallet
+            </a>
+            <a>
+                Liquidity
+            </a>
+        </div>
+        <div className="row">
+            USDC Market
+        </div>
     </div>
 }
 
