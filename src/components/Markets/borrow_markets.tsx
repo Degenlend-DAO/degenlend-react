77490cbda940904cefad5acf49a69bd1c226be0a
Kevin Kamto <kamtokevin@gmail.com>
2024-01-04 18:11:23 -0500

    refactor: updated headers

--- src/components/Markets/borrow_markets.tsx
+++ src/components/Markets/borrow_markets.tsx
@@ -14,25 +14,28 @@ const BorrowMarkets = () => <>{
 <Col span={12}>
     <Card bordered={true} title="Borrow Markets">
         <Row>
-        <div className="row">
-            <a>
-                Asset
-            </a>
-            <a>
-                APY
-            </a>
-            <a>
-                Wallet
-            </a>
-            <a>
-                Liquidity
-            </a>
-        </div>
-        </Row>
-        <Row>
             <List itemLayout='horizontal'  
+            header={
+                <Row>
+                
+                    <h4>
+                        Asset
+                    </h4>
+                    <h4>
+                        APY
+                    </h4>
+                    <h4>
+                        Wallet
+                    </h4>
+                    <h4>
+                        Liquidity
+                    </h4>
+                
+                </Row>
+            }
             dataSource={borrowMarkets}
-            renderItem={(item) => <List.Item>
+            renderItem={(item) => 
+            <List.Item>
                 <List.Item.Meta 
                 title={ <h4>{item.title}</h4>} 
                 description={<p>
