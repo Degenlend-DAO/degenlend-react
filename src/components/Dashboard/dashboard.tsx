985043719c9f62fb018230d8bd0223e28a6f3808
Kevin Kamto <kamtokevin@gmail.com>
2024-01-03 23:12:29 -0500

    chore: setup basic dashboard

---
+++ src/components/Dashboard/dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Dashboard = () => {
+    <div> 
+        This is the dashboard containing Supply Balance, Net APY, and Borrow Balance
+    </div>
+}
+
+export default Dashboard
