df8fc18d874ef35ee7d6362b666856df1f0e8acd
Kevin Kamto <kamtokevin@gmail.com>
2024-01-04 12:16:10 -0500

    chore: supply markets structure setup

--- src/components/Markets/supply_markets.tsx
+++ src/components/Markets/supply_markets.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
 const SupplyMarkets = () => {
-    <div>
-        This is the supply markets including SX token, and eventually AX token
+    <div className="column">
+        <h3>
+            Supply Markets
+        </h3>
+        <div className="row">
+            <a>
+                Asset
+            </a>
+            <a>
+                APY
+            </a>
+            <a>
+                Wallet
+            </a>
+            <a>
+                Collateral
+            </a>
+        </div>
+        <div className="row">
+            SX Token Market
+            </div>
     </div>
 }
 
