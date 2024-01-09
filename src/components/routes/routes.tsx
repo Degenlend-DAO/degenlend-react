23a211b48f51d90c9712f5199c025e9a39e2965d
Kevin Kamto <kamtokevin@gmail.com>
2024-01-08 19:06:15 -0700

    feat: route points to dashboard

--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import SupplyMarkets from '../Markets/supply_markets'
+import Dashboard from '../Dashboard/dashboard'
+import Root from '../../views/root'
 
 const RoutesComponent = () => {
     return (
         <Routes>
-            <Route path="/" element={<div> The Dashboard goes here! </div>} />
-            <Route path="/markets" element={<div>  View the markets details here!</div>} />
-            <Route path="/vote" element={<div> Go here to Vote!</div>} />
+            <Route path="/" element={<Root />} />
         </Routes>
     )
 }
