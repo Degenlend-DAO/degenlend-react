130b0a39b82a2079327e02c214e6c41175a66873
Kevin Kamto <kamtokevin@gmail.com>
2024-01-11 17:10:51 -0700

    feat: removed markets menu item

--- src/components/Header/topNavigationBar.tsx
+++ src/components/Header/topNavigationBar.tsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
-import type { MenuProps } from 'antd';
-import { Menu } from 'antd';
+import { Menu, MenuProps, Col, Button, Modal, Card } from 'antd';
+import { CaretRightOutlined, WalletFilled } from '@ant-design/icons';
+
+const { confirm } = Modal;
 
 const items: MenuProps['items'] = [
     {
         label: 'Dashboard',
         key: 'dashboard'
     },
-    {
-        label: 'Markets',
-        key: 'markets'
-    },
+
+    ///Markets may be included in the future
+    // {
+    //     label: 'Markets',
+    //     key: 'markets'
+    // },
     {
         label: (
             <a href="https://snapshot.org/#/sportx.eth"  target="_blank" rel="noopener noreferrer">
@@ -20,13 +24,26 @@ const items: MenuProps['items'] = [
     },
 ]
 
-const TopNavigationBar = () => {
+
+
+const TopNavigationBar: React.FC = () => {
     const [ current, setCurrent ] = useState('dashboard');
     const onClick: MenuProps['onClick'] = (e) => {
         console.log('click ', e);
         setCurrent(e.key);
     }
-    return <Menu onClick={onClick} theme='dark' selectedKeys={[current]} mode="horizontal" defaultSelectedKeys={['2']} items={items} style={{ flex: 1, minWidth: 0 }}/>;
+    return <>
+        <Col span={6}>
+        <h2> <a href="https://sx.technology/">DegenLend</a></h2>
+        </Col>
+        <Menu onClick={onClick} theme='dark' selectedKeys={[current]} mode="horizontal" defaultSelectedKeys={['2']} items={items} style={{ flex: 1, minWidth: 0 }}/>
+        <Col span={6}>
+        <Button type="default" shape="round" icon={<CaretRightOutlined />} size="large">
+            Connect Wallet
+        </Button>
+
+        </Col>
+     </>
 }
 
 export default TopNavigationBar
