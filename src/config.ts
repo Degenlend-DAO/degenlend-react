a42cbcbeef7397981abf7a41abf3ee72dba9e682
Kevin Kamto <kamtokevin@gmail.com>
2024-01-15 22:07:06 -0500

    chore: added config dependency

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+
+
+const config = {
+    /**
+        * ACCOUNT
+    */
+  PRIVATE_KEY: process.env.PRIVATE_KEY!,
+  PUBLIC_KEY: process.env.PUBLIC_KEY!,
+
+  /**
+   * NETWORK
+   */
+    CHAIN_ID: 647, //Toronto Testnet
+
+    
+
+}
+
+export default config
