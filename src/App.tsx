91259d96173ba22a727980d0e1c22283e8fdffe6
rbharath99 <dbossman60@gmail.com>
2024-01-17 22:20:16 -0500

    chore: removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom'
 import RoutesComponent from './components/routes/Routes';
