bf25ec549e6df60261e1852fbcc89edb0b4ad499
rbharath99 <dbossman60@gmail.com>
2024-01-18 00:53:05 -0500

    chore: renaming folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom'
-import RoutesComponent from './components/routes/Routes';
+import RoutesComponent from './components/Routes/Routes';
 import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
 
 import { WagmiConfig } from 'wagmi'
