506ee2e0d1c4e3575b5b7e51a16b531cabb599ca
Kevin Kamto <kamtokevin@gmail.com>
2024-01-22 14:21:58 -0500

    feat: basic supply markets

--- src/components/Markets/SupplyMarkets.tsx
+++ src/components/Markets/SupplyMarkets.tsx
@@ -1,34 +1,62 @@
 import React from 'react';
-import { Avatar, Button, List, Col, Card, Row } from 'antd';
+import { Avatar, Button, Modal, Col, Card, Row, Space, Switch,  } from 'antd';
+import { Provider, useStore } from 'react-redux';
 
-const supplyMarkets = [
-    {
-        title: "SX Token",
-        description: "The token for SX blockchain",
-        apy: "5%",
-        walletBalance: "0",
-        liquidity: ""
-    },
-    // {
-    //     title: "AthleteX Token",
-    //     description: "The token for AthleteX Markets",
-    //     apy: "8.7%",
-    //     walletBalance: "",
-    //     liquidity: ""
-    // }
-]
 
-const loadItemDetails = () => {
-    alert('do better!');
+const { info } = Modal;
+
+const wsxMarketSelected = () => {
+
+    info({
+        title: "About WSX Token",
+        okText: "Return",
+        centered: false,
+        closeIcon: true,
+        maskClosable: true,
+        content: <SXNetworkCard />,
+    })
 }
 
-const SupplyMarkets = () => <>{
+const SXNetworkCardTitle = () => {
+    return <><div><img></img> Wrapped SX Token</div></>;
+}
+
+const enableWSXLending = () => {
+    // This is where actions go for the erc20 token (enable useage)
+    alert('You can now borrow your WSX!');
+}
+
+const SXNetworkCard = () => {
+    return <>
+        <div style={{ display: "flex", alignItems: "center"}}>
+        {/* SX Token IMG */}
+            <Row>
+                <img src='https://s2.coinmarketcap.com/static/img/coins/64x64/8377.png' alt='WSX Token'></img>
+            </Row>
+        </div>
+            <Switch checkedChildren="  Supply  " unCheckedChildren="  Withdraw  " defaultChecked/>
+            <Space />
+            
+            <Button onClick={enableWSXLending}>
+                Enable SX Token 
+            </Button>
+
+            <Row>
+                <h4>Your Wallet Balance: </h4>
+                <h4> 0 WSX </h4>
+            </Row>
+        
+    </>;
+}
+
+const SupplyMarkets:React.FC = () => <>{
 
     <div className="column">
-
         <Col>
-            <Card bordered={true} title="Supply Markets">
-                <p>These markets are available for you to borrow against</p>
+            <Card bordered={true} title="Supply Markets" style={{ width: 350 }} onClick={wsxMarketSelected}>
+                <Card hoverable>
+                        <img src='https://s2.coinmarketcap.com/static/img/coins/64x64/8377.png'></img>
+                </Card>
             </Card>
         </Col>
     </div>
