ec6cb6e3bfdbda99b61ac7bc27a75477ee0445d1
Kevin Kamto <kamtokevin@gmail.com>
2024-01-22 14:24:58 -0500

    chore: minor styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom'
-import RoutesComponent from './components/Routes/routes';
+import RoutesComponent from './components/routes/routes';
 import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
-
 import { WagmiConfig } from 'wagmi'
 import { arbitrum, mainnet } from 'viem/chains'
 
 // 1. Get projectId at https://cloud.walletconnect.com
-const projectId = 'YOUR_PROJECT_ID'
+const projectId = ''
 
 // 2. Create wagmiConfig
 const metadata = {
-  name: 'Web3Modal',
-  description: 'Web3Modal Example',
+  name: 'DegenLend',
+  description: 'DegenLend - Bet more',
   url: 'https://web3modal.com',
   icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
