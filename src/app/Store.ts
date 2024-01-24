e0f6842f13c6ca18414cc86c8acd9b59702c1725
Kevin Kamto <kamtokevin@gmail.com>
2024-01-24 13:25:17 -0500

    chore: added additional reducers

--- src/app/Store.ts
+++ src/app/Store.ts
@@ -1,12 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import metaMaskReducer from '../feature/wallet/MetaMaskSlice';
 import walletConnectReducer from '../feature/wallet/WalletConnectSlice';
+import supplyBalanceReducer from '../feature/dashboard/supplyBalanceSlice';
+import borrowBalanceReducer from '../feature/dashboard/borrowBalanceSlice';
+import borrowLimitReducer from '../feature/dashboard/borrowLimitSlice';
+import netAPYReducer from '../feature/dashboard/netAPYSlice';
 import { useDispatch } from "react-redux";
 
 export const store = configureStore({
     reducer: {
         metaMask: metaMaskReducer,
         walletConnect: walletConnectReducer,
+        supplyBalance: supplyBalanceReducer,
+        borrowBalance: borrowBalanceReducer,
+        borrowLimit: borrowLimitReducer,
+        netAPY: netAPYReducer,
     },
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware({
