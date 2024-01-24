b2a26389fbb55d40ec49d9f27fc4419854772652
Kevin Kamto <kamtokevin@gmail.com>
2024-01-24 14:33:22 -0500

    refactor: simplified cards

--- src/components/WalletCard/SXTokenCardContent.tsx
+++ src/components/WalletCard/SXTokenCardContent.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Row, Switch, Divider, Button, Col, Skeleton } from 'antd';
+import { Flex, Card, Row, Radio, Divider, Button, Col, Form } from 'antd';
 import { approveWSX } from '../../feature/supply/withdrawWSXSlice';
 import { useDispatch } from 'react-redux';
 
@@ -10,6 +10,10 @@ const enableWSXLending = () => {
 }
 
 const SXNetworkCardContent: React.FC = () => {
+    const [form] = Form.useForm();
+
+    const [ selectedActivity, setSelectedActivity ] = useState("supply")
+
     return <div>
         <div style={{ display: "flex", justifyItems: "center"}}>
             <Col offset={11}>
@@ -21,8 +25,20 @@ const SXNetworkCardContent: React.FC = () => {
         <Row gutter={4}>
             <p>To supply, withdraw, or repay your Wrapped SX, you need to enable it first</p>
         </Row>
+        <Flex vertical align='center'>
+                <Form
+                    form={form}>
+                            <Radio.Group defaultValue="supply" size='large' buttonStyle='solid'>
+                                <Radio.Button value="supply">Supply</Radio.Button>
+                                <Radio.Button value="withdraw">Withdraw</Radio.Button>
+                            </Radio.Group>
+                </Form>
 
-        
+            
+
+        </Flex>
+
+
     </div>
 }
 
--- src/components/WalletCard/USDCTokenCardContent.tsx
+++ src/components/WalletCard/USDCTokenCardContent.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Row, Switch, Divider, Button, Col, Skeleton } from 'antd';
+import { Card, Row, Flex, Radio, Form, Divider, Button, Col, Skeleton } from 'antd';
 
 
 const enableUSDCLending = () => {
@@ -7,6 +7,9 @@ const enableUSDCLending = () => {
 }
 
 const USDCCardContent: React.FC = () => {
+
+    const [form] = Form.useForm();
+
     return <div>
         <div style={{ display: "flex", justifyItems: "center" }}>
             <Col offset={11}>
@@ -18,15 +21,16 @@ const USDCCardContent: React.FC = () => {
         <Row gutter={4}>
             <p>To supply, withdraw or repay your USDC, you need to enable it first</p>
         </Row>
+        <Flex vertical align='center'>
+            <Form form={form}>
+            <Radio.Group defaultValue="borow" size='large' buttonStyle='solid'>
+                            <Radio.Button value="borow">Borrow</Radio.Button>
+                            <Radio.Button value="repay">Repay</Radio.Button>
+                </Radio.Group>
+            </Form>
 
-        <Row gutter={6}>
-            <Col span={6}>
 
-            </Col>
-            <Col span={6}>
-            
-            </Col>
-        </Row>
+        </Flex>
     </div>
 }
 
