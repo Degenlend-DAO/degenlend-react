fb73dbc803a81b479cffde4fd4636bb6a49776fd
Kevin Kamto <kamtokevin@gmail.com>
2024-01-25 23:12:47 -0500

    feat: setup of users usdcbalance

--- src/feature/borrow/repayUSDCSlice.ts
+++ src/feature/borrow/repayUSDCSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { Web3 } from 'web3';
 
 interface RepayUSDCState {
     status: string,
@@ -8,6 +9,15 @@ const initialState: RepayUSDCState = {
     status: 'initial',
 }
 
+export const updateUSDCBalance = createAsyncThunk(
+    'usdcBalance/update',
+    async () => {
+        const web3 = new Web3('https://rpc.toronto.sx.technology/');          
+          const balance = web3.utils.fromWei(await web3.eth.getBalance('0x4869aF0Aed0a9948f724f809dC0DCcF9885cCe34'), "ether");
+        return balance;
+    }
+);
+
 export const approveUSDC = createAsyncThunk('usdc/approve', async () => {});
 
 export const confirmUSDC = createAsyncThunk('usdc/confirm', async () => {});
@@ -16,7 +26,11 @@ export const repayUSDCSlice = createSlice({
     name: "repayUSDC",
     initialState,
     reducers: {},
-    extraReducers: (builder) => {}
+    extraReducers: (builder) => {
+        builder.addCase(updateUSDCBalance.fulfilled, (state, action) => {
+
+        })
+    }
 });
 
 export default repayUSDCSlice.reducer;
