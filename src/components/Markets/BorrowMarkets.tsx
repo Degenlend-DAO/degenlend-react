12fa3e8e920d4d65d592e676b7008d7094ccd06f
Kevin Kamto <kamtokevin@gmail.com>
2024-01-25 23:12:16 -0500

    refactor: added a provider to popup & rewrote component

--- src/components/Markets/BorrowMarkets.tsx
+++ src/components/Markets/BorrowMarkets.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Modal, Card, Col } from 'antd';
 import USDCCardContent from '../WalletCard/USDCTokenCardContent';
+import { Provider, useStore, useDispatch } from 'react-redux';
 
 
+
+const BorrowMarkets = () => {
+
+    const store = useStore();
 const { info } = Modal;
 
 const usdcMarketSelected = () => {
@@ -13,25 +18,24 @@ const usdcMarketSelected = () => {
         closeIcon: true,
         maskClosable: true,
         width: 810,
-        content: <USDCCardContent />,
+        content: <Provider store={store}><USDCCardContent /></Provider>,
     })
 }
 
+    return (
+        <div className="column">
+            <Col>
+                <Card bordered={true} title="Borrow Markets" onClick={usdcMarketSelected} style={{ width: 450 }}>
 
-const BorrowMarkets = () => <>{
+                    <Card hoverable>
+                        <img width="64" height="64" src="https://tokensinvaders.com/wp-content/uploads/2021/02/usd-coin-usdc-logo-1024x1024.png"></img>
+                    </Card>
 
-    <div className="column">
-        <Col>
-            <Card bordered={true} title="Borrow Markets" onClick={usdcMarketSelected} style={{ width: 450 }}>
-
-                <Card hoverable>
-                    <img width="64" height="64" src="https://tokensinvaders.com/wp-content/uploads/2021/02/usd-coin-usdc-logo-1024x1024.png"></img>
                 </Card>
+            </Col>
+        </div>
+    );
 
-            </Card>
-        </Col>
-    </div>
-
-}</>
+}
 
 export default BorrowMarkets
