80d005d1bad3c90d83494f41d4acf747edab4eb1
Kevin Kamto <kamtokevin@gmail.com>
2024-01-25 23:16:14 -0500

    chore: added reducer to update borrow limit

--- src/feature/dashboard/borrowLimitSlice.ts
+++ src/feature/dashboard/borrowLimitSlice.ts
@@ -13,14 +13,19 @@ export const updateborrowLimit = createAsyncThunk(
     'borrowLimit/update',
     async () => {
         // Contract call
-        return 0;
+        return 1;
     }
 );
 
 export const borrowLimitSlice = createSlice({
     name: "borrowLimit",
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(updateborrowLimit.fulfilled, (state, action) => {
+            state.borrowLimit = action.payload || 0;
+        })
+    }
 });
 
 export default borrowLimitSlice.reducer;
