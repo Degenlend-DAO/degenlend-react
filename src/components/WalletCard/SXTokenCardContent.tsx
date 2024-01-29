eec028bc030417892439a325004f8316fdac1db2
Kevin Kamto <kamtokevin@gmail.com>
2024-01-29 14:26:43 -0500

    feat: replaced form with segmented

--- src/components/WalletCard/SXTokenCardContent.tsx
+++ src/components/WalletCard/SXTokenCardContent.tsx
@@ -1,26 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { Flex, Card, Row, Radio, Divider, Button, Col, Form, Statistic } from 'antd';
+import { Flex, Card, Row, Radio, Divider, Button, Col, Form, Statistic, Segmented } from 'antd';
 import {  useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../app/Store';
-import { approveWSX, updateWSXBalance } from '../../feature/supply/supplyWSXSlice';
+import { approveWSX, updateWSXBalance, updatewsxSupplyAPY } from '../../feature/supply/WSXSlice';
 import { UnknownAction } from '@reduxjs/toolkit';
 
 const SXNetworkCardContent: React.FC = () => {
     const dispatch = useDispatch();
     const [form] = Form.useForm();
 
-    const wsxBalance = useSelector((state:RootState) => state.supplyWSX.wsxBalance);
-    const supplyAPY = useSelector((state:RootState) => state.supplyWSX.supplyAPY);;
-
+    const wsxBalance = useSelector((state:RootState) => state.WSX.wsxBalance);
+    const supplyAPY = useSelector((state:RootState) => state.WSX.supplyAPY);;
+    let isSupply = false;
     const enableWSXLending = () => {
         dispatch(approveWSX() as unknown as UnknownAction);
     }
 
     useEffect(() => {
+        dispatch( updatewsxSupplyAPY() as unknown as UnknownAction );
         dispatch( updateWSXBalance() as unknown as UnknownAction);
     })
 
-    return <div>
+    return (<div>
         <div style={{ display: "flex", justifyItems: "center"}}>
             <Col offset={11}>
                 <img src='https://s2.coinmarketcap.com/static/img/coins/64x64/8377.png' alt='WSX Token'></img>
@@ -32,23 +33,18 @@ const SXNetworkCardContent: React.FC = () => {
             <p>To supply, withdraw, or repay your Wrapped SX, you need to enable it first</p>
         </Row>
         <Flex vertical align='center'>
-                <Form
-                    form={form}>
-                            <Radio.Group defaultValue="supply" size='large' buttonStyle='solid'>
-                                <Radio.Button value="supply">Supply</Radio.Button>
-                                <Radio.Button value="withdraw">Withdraw</Radio.Button>
-                            </Radio.Group>
-                </Form>
-
-                    <Card bordered={false}>
+                        <Segmented
+                                defaultValue="center"
+                                style={{ marginBottom: 8 }}
+                                onChange={() => {isSupply = !isSupply}}
+                                options={['Supply', 'Withdraw']}
+                                />
                         <Statistic
                         title="Supply APY"
                         value={supplyAPY}
                         precision={2}
                         suffix="%"
                         />
-                    </Card>
-
 
                 <Button type="primary" size={'large'} onClick={enableWSXLending}>
                     Enable WSX
@@ -58,6 +54,7 @@ const SXNetworkCardContent: React.FC = () => {
                 </p>
         </Flex>
     </div>
+    );
 }
 
 export default SXNetworkCardContent;
