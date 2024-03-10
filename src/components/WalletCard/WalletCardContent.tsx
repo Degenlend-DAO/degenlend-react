273d6ee28c668dc589c47e5f72f9a3559efe5f68
rbharath99 <dbossman60@gmail.com>
2024-03-10 15:34:27 -0400

    refactor: metamask and walletconnect logo local images for wallet card and wallet card contents

--- src/components/WalletCard/WalletCardContent.tsx
+++ src/components/WalletCard/WalletCardContent.tsx
@@ -1,33 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { Card, Row, Col, Skeleton } from 'antd';
+import metamasklogo from '../../assets/metamask-logo.jpg';
+import walletconnectlogo from '../../assets/walletconnect-logo.png';
 
 const WalletCardContent: React.FC = () => {
   const [isActive, setActive] = useState<boolean>(false);
 
   return (
     <div>
-        {isActive ?
-          <div>
-            <Row>
-              <Col span={6}>
-                <Skeleton active style={{ width: 240, height: 130 }} />
-              </Col>
-              <Col span={6} offset={6}>
-                <Skeleton active style={{ width: 240, height: 130 }} />
-              </Col>
-            </Row>
-          </div>
-          : <div>
-            <Row>
-              <Col span={6}>
-                <Card bordered={true} hoverable style={{ width: 240, height: 130 }} cover={<img alt="Metamask" src="http://tinyurl.com/8jttsvbw"></img>}></Card>
-              </Col>
-              <Col span={6} offset={6}>
-                <Card bordered={true} hoverable style={{ width: 240, height: 130 }} cover={<img alt="WalletConnect" src="http://tinyurl.com/2smfxt43"></img>}></Card>
-              </Col>
-            </Row>
-          </div>
-        }
+      {isActive ?
+        <div>
+          <Row>
+            <Col span={6}>
+              <Skeleton active style={{ width: 240, height: 130 }} />
+            </Col>
+            <Col span={6} offset={6}>
+              <Skeleton active style={{ width: 240, height: 130 }} />
+            </Col>
+          </Row>
+        </div>
+        : <div>
+          <Row>
+            <Col span={6}>
+              <Card bordered={true} hoverable style={{ width: 240, height: 130 }} cover={<img alt="Metamask" src={metamasklogo}></img>}></Card>
+            </Col>
+            <Col span={6} offset={6}>
+              <Card bordered={true} hoverable style={{ width: 240, height: 130 }} cover={<img alt="WalletConnect" src={walletconnectlogo}></img>}></Card>
+            </Col>
+          </Row>
+        </div>
+      }
     </div>
   );
 }
