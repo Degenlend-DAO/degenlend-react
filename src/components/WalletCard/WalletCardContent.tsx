d323f795f3fea1b1d2ec215de252da1c61ab290f
rbharath99 <dbossman60@gmail.com>
2024-03-10 16:53:48 -0400

    chore: format wallet card content

--- src/components/WalletCard/WalletCardContent.tsx
+++ src/components/WalletCard/WalletCardContent.tsx
@@ -6,28 +6,28 @@ const WalletCardContent: React.FC = () => {
 
   return (
     <div>
-        {isActive ?
-          <div>
-            <Row>
-              <Col span={6}>
-                <Skeleton active style={{ width: 240, height: 130 }} />
-              </Col>
-              <Col span={6} offset={6}>
-                <Skeleton active style={{ width: 240, height: 130 }} />
-              </Col>
-            </Row>
-          </div>
-          : <div>
-            <Row>
-              <Col span={6}>
-                <Card bordered={true} hoverable style={{ width: 240, height: 130 }} cover={<img alt="Metamask" src="http://tinyurl.com/8jttsvbw"></img>}></Card>
-              </Col>
-              <Col span={6} offset={6}>
-                <Card bordered={true} hoverable style={{ width: 240, height: 130 }} cover={<img alt="WalletConnect" src="http://tinyurl.com/2smfxt43"></img>}></Card>
-              </Col>
-            </Row>
-          </div>
-        }
+      {isActive ?
+        <div>
+          <Row>
+            <Col span={6}>
+              <Skeleton active style={{ width: 240, height: 130 }} />
+            </Col>
+            <Col span={6} offset={6}>
+              <Skeleton active style={{ width: 240, height: 130 }} />
+            </Col>
+          </Row>
+        </div>
+        : <div>
+          <Row>
+            <Col span={6}>
+              <Card bordered={true} hoverable style={{ width: 240, height: 130 }} cover={<img alt="Metamask" src="http://tinyurl.com/8jttsvbw"></img>}></Card>
+            </Col>
+            <Col span={6} offset={6}>
+              <Card bordered={true} hoverable style={{ width: 240, height: 130 }} cover={<img alt="WalletConnect" src="http://tinyurl.com/2smfxt43"></img>}></Card>
+            </Col>
+          </Row>
+        </div>
+      }
     </div>
   );
 }
