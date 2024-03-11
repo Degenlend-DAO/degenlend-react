f59c6456176b67fc7b8e2a75785ceb19d8d73244
rbharath99 <dbossman60@gmail.com>
2024-03-11 16:36:57 -0400

    chore: formatting supply markets

--- src/components/Markets/SupplyMarkets.tsx
+++ src/components/Markets/SupplyMarkets.tsx
@@ -1,40 +1,37 @@
 import React from 'react';
-import { Avatar, Switch, Modal, Col, Card, Tooltip,  } from 'antd';
+import { Switch, Modal, Col, Card, Tooltip, } from 'antd';
 import SXTokenCardContent from '../WalletCard/SXTokenCardContent';
-import { Provider, useStore, useDispatch } from 'react-redux';
+import { Provider, useStore } from 'react-redux';
 import sxtoken from '../../assets/sx_coin_token.png';
 
 
-const SupplyMarkets:React.FC = () => {
+const SupplyMarkets: React.FC = () => {
 
     const store = useStore();
     const { info } = Modal;
 
     const wsxMarketSelected = () => {
         info({
-            title:    <div style={{ display: "flex", justifyContent: "space-between"}}>
-            <><div><Tooltip title="Leverage your WSX and borrow or trade against it">About WSX Token</Tooltip></div><div><Tooltip title="Use the switch to enable wSX as collateral on the platform"><Switch title='Enable or disable this cryptocurrency on the protocol' checkedChildren='Enabled' /></Tooltip></div></> </div>,
+            title: <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <><div><Tooltip title="Leverage your WSX and borrow or trade against it">About WSX Token</Tooltip></div><div><Tooltip title="Use the switch to enable wSX as collateral on the platform"><Switch title='Enable or disable this cryptocurrency on the protocol' checkedChildren='Enabled' /></Tooltip></div></> </div>,
             okText: "Return",
             centered: true,
             closeIcon: true,
             maskClosable: true,
             width: 810,
-            
             content: <Provider store={store}><SXTokenCardContent /></Provider>,
         });
     }
 
 
     return (
-        <div className="column">
         <Col>
             <Card bordered={true} title="Supply Markets" style={{ width: 450 }} onClick={wsxMarketSelected}>
                 <Card hoverable>
-                        <img src={sxtoken} alt='WSX Token'></img>
+                    <img src={sxtoken} alt='WSX Token'></img>
                 </Card>
             </Card>
         </Col>
-    </div>
     );
 }
 
