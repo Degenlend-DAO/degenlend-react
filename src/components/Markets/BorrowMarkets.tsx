23cb4d61c96efbb58e46ad5839ba1e51b081eecf
Kevin Kamto <kamtokevin@gmail.com>
2024-03-12 19:51:40 -0400

    feat: add wsx token card

--- src/components/Markets/BorrowMarkets.tsx
+++ src/components/Markets/BorrowMarkets.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Modal, Card, Col, Tooltip } from 'antd';
-import USDCCardContent from '../WalletCard/USDCTokenCardContent';
+import USDCCardContent from '../WalletCard/Borrow Markets/USDCTokenCardContent';
 import { Provider, useStore } from 'react-redux';
 import usdctoken from '../../assets/usd-coin-usdc-logo-64x64.png';
+import wsxtoken from '../../assets/sx_coin_token.png';
+import BorrowWSXCardContent from '../WalletCard/Borrow Markets/WSXTokenCardContent';
 
 const BorrowMarkets: React.FC = () => {
 
@@ -11,7 +13,7 @@ const BorrowMarkets: React.FC = () => {
 
     const usdcMarketSelected = () => {
         info({
-            title: <Tooltip title="Leverage your USDC and borrow or trade against it">About USDC Token</Tooltip>,
+            title: <Tooltip title="Leverage your USDC and borrow or trade against it">About Borrowing USDC Token</Tooltip>,
             okText: "Return",
             centered: true,
             closeIcon: true,
@@ -21,15 +23,29 @@ const BorrowMarkets: React.FC = () => {
         })
     }
 
+    const wsxMarketSelected = () => {
+        info({
+            title: <Tooltip title="Leverage your WSX and borrow or trade against it">About Borrowing WSX Token</Tooltip>,
+            okText: "Return",
+            centered: true,
+            closeIcon: true,
+            maskClosable: true,
+            width: '50vw',
+            content: <Provider store={store}><BorrowWSXCardContent /></Provider>,
+        })
+    }
+
     return (
         <Card
-            hoverable
             bordered={true}
             title="Borrow Markets"
             headStyle={{ backgroundColor: 'rgba(37, 102, 216, 1)', border: 0 }}
-            onClick={usdcMarketSelected}>
-            <Card>
+            >
+            <Card onClick={usdcMarketSelected} hoverable>
                 <img width="64" height="64" src={usdctoken} alt='USDC Token'></img>
+            </Card>
+            <Card onClick={wsxMarketSelected} hoverable >
+                <img width="64" height="64" src={wsxtoken} alt='Wrapped SX Token'></img>
             </Card>
         </Card>
     );
