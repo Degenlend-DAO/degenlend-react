62701c50650a94fb714df15d3378bc5587e1839a
Kevin Kamto <kamtokevin@gmail.com>
2024-03-13 10:53:18 -0400

    chore: setup basic usdc supply markets

---
+++ src/components/WalletCard/Supply Markets/USDCTokenCardContent.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import { Flex, Card, Row, Radio, Divider, Button, Col, Form, Statistic, Segmented, Input, Tooltip, Space, InputNumber } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../../app/Store';
+
+
+const SupplyUSDCCardContent: React.FC = () => {
+
+
+    return (
+        <div style={{ textAlign: "center"}}>
+            <Row justify="center" align="middle" style={{ marginBottom: 20 }}>
+
+            </Row>
+        </div>
+    );
+}
+
+export default SupplyUSDCCardContent;
