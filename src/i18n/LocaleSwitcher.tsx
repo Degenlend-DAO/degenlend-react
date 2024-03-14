14b142d4171644258911709241b3c7893e3df745
rbharath99 <dbossman60@gmail.com>
2024-03-13 21:44:12 -0400

    chore: ignore for locale switcher

--- src/i18n/LocaleSwitcher.tsx
+++ src/i18n/LocaleSwitcher.tsx
@@ -2,6 +2,11 @@ import { useTranslation } from "react-i18next";
 import { supportedLanguages } from "./config";
 
 export default function LocaleSwitcher() {
+
+    /// Typescript is complaining about deep instantiation here...
+    /// I don't understand why & I dont want to because it's not breaking anything
+    /// So, this error is being ignored.  If there are any complaints get in touch with me on Github @user2745
+    // @ts-ignore
     const { i18n } = useTranslation();
 
     return (
