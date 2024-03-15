e2b263736544e1c0fd7b337cfdae63ef1e20d5ed
Kevin Kamto <kamtokevin@gmail.com>
2024-03-15 12:12:34 -0400

    USD constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,6 @@
 export const EMPTY_ADDRESS = "0x0000000000000000000000000000000000000000";
+export const USDollar = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
+export const WSX = new Intl.NumberFormat('en-US');
