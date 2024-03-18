9f0f801bcc4de0ced7da8a9428ff99246402eb73
Kevin Kamto <kamtokevin@gmail.com>
2024-03-18 11:47:22 -0400

    chore: added mantissa to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
-export const EMPTY_ADDRESS = "0x0000000000000000000000000000000000000000";
+export const EMPTY_ADDRESS:string = "0x0000000000000000000000000000000000000000";
+export const MANTISSA:number = 1000000000000000000;
 export const USDollar = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
